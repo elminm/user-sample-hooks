b0982d45bdd3e91660187d6f8c78e6e44abef2e1
elminm <elmin555@bk.ru>
2023-05-06 18:27:08 +0400

    add spinner

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,48 +2,86 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Table } from "reactstrap";
+import { Spinner, Table } from "reactstrap";
 
 const Posts = ({ selectedId }) => {
-  const [posts, setPosts] = useState([]);
-  const [filtered, setFiltered] = useState([]);
-
+  const [datas, setDatas] = useState({
+    posts: [],
+    filtered: [],
+    loading: true,
+    error: undefined,
+  });
   useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts`).then(({ data }) => {
-      setPosts(data);
-      setFiltered(data);
-    });
+    setDatas((oldData) => ({
+      ...oldData,
+      loading: true,
+      error: undefined,
+      filtered: undefined,
+      posts: undefined,
+    }));
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(({ data }) => {
+        setDatas((oldData) => ({
+          ...oldData,
+          filtered: data,
+          posts: data,
+          loading: false,
+          error: undefined,
+        }));
+      })
+      .catch((err) => {
+        setDatas({
+          filtered: undefined,
+          posts: undefined,
+          loading: false,
+          error: err.message,
+        });
+      });
   }, []);
 
   useEffect(() => {
-    console.log(selectedId);
     if (selectedId == 0) {
-      setFiltered([...posts]);
+      setDatas((oldData) => ({
+        ...oldData,
+        filtered: [...datas.posts],
+      }));
     } else {
-      const filteredPosts = posts.filter(({ userId }) => userId == selectedId);
-      setFiltered([...filteredPosts]);
+      const filteredPosts = datas.posts.filter(
+        ({ userId }) => userId == selectedId
+      );
+      setDatas((oldData) => ({
+        ...oldData,
+        filtered: [...filteredPosts],
+      }));
     }
   }, [selectedId]);
   return (
     <>
-      <Table hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Title</th>
-            <th>Body</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filtered?.map(({ id, title, body }) => (
-            <tr key={id}>
-              <th scope="row">{id}</th>
-              <td>{title}</td>
-              <td>{body}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      {datas.error && <h1>Error 404...</h1>}
+      {datas.loading && <Spinner>Loading...</Spinner>}
+      {datas.filtered && (
+        <>
+          <Table hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Body</th>
+              </tr>
+            </thead>
+            <tbody>
+              {datas.filtered?.map(({ id, title, body }) => (
+                <tr key={id}>
+                  <th scope="row">{id}</th>
+                  <td>{title}</td>
+                  <td>{body}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
     </>
   );
 };
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,58 +1,73 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Form, FormGroup, Input, Label } from "reactstrap";
+import { Form, FormGroup, Input, Label, Spinner } from "reactstrap";
 
 const Users = ({ handleSelect }) => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState({
+    data: undefined,
+    loading: true,
+    error: undefined,
+  });
   useEffect(() => {
+    setUsers((prev) => ({
+      ...prev,
+      data: undefined,
+      loading: true,
+      error: undefined,
+    }));
     axios
       .get("https://jsonplaceholder.typicode.com/users")
-      .then(({ data }) => setUsers(data));
+      .then(({ data }) =>
+        setUsers((prev) => ({
+          ...prev,
+          data,
+          loading: false,
+          error: undefined,
+        }))
+      )
+      .catch((err) => {
+        setUsers({
+          filtered: undefined,
+          posts: undefined,
+          loading: false,
+          error: err.message,
+        });
+      });
   }, []);
 
   return (
     <>
-      <Form>
-        <FormGroup>
-          <Label for="exampleSelect">Users</Label>
-          <Input
-            id="exampleSelect"
-            name="select"
-            type="select"
-            onChange={(e) => handleSelect(e.target.value)}
-          >
-            <optgroup label="Select User">
-              <option value="0">All Users</option>
-              {users.map(({ id, name }) => (
-                <option key={id} value={id}>
-                  {name}
-                </option>
-              ))}
-            </optgroup>
-          </Input>
-        </FormGroup>
-      </Form>
+      {users.error && <h1>Users not Found 404...</h1>}
+      {users.loading && <Spinner>Loading...</Spinner>}
+      {users.data && (
+        <>
+          {" "}
+          <Form>
+            <FormGroup>
+              <Label for="exampleSelect">Users</Label>
+              <Input
+                id="exampleSelect"
+                name="select"
+                type="select"
+                onChange={(e) => handleSelect(e.target.value)}
+              >
+                <optgroup label="Select User">
+                  <option value="0">All Users</option>
+                  {users &&
+                    users?.data?.map(({ id, name }) => (
+                      <option key={id} value={id}>
+                        {name}
+                      </option>
+                    ))}
+                </optgroup>
+              </Input>
+            </FormGroup>
+          </Form>
+        </>
+      )}
     </>
   );
 };
 
 export default Users;
-
-{
-  /* <FormGroup>
-          <Label for="exampleSelectMulti">Select Multiple</Label>
-          <Input
-            id="exampleSelectMulti"
-            multiple
-            name="selectMulti"
-            type="select"
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-        </FormGroup> */
-}
